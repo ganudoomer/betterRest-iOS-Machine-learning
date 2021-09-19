d60af87da769c05637a83dc20c71d661d098cfa2
ganudoomer <ganuiscool@gmail.com>
2021-09-19 11:03:25 +0530

    Initial Commit

---
+++ betterRest/betterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterRestApp.swift
+//  betterRest
+//
+//  Created by Sree on 19/09/21.
+//
+
+import SwiftUI
+
+@main
+struct betterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
